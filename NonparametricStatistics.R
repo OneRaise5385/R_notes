33f84daf7e8131583aa76587ccb89949f8b75728
OneRaise <zh1372337794@163.com>
2023-09-22 21:24:20 +0800

    update

--- NonparametricStatistics.R
+++ NonparametricStatistics.R
@@ -73,7 +73,7 @@ mean(vector_1)
 median(vector_1)  # 中位数
 var(vector_1)  # 方差
 sd(vector_1)  # 标准差
-rank(vector_1)
+rank(c(9,2,4))  # 排序，输出原来元素对应的次序
 
 # 向量的逻辑运算，比较短的向量的元素之间的大小
 vector_1 < vector_2
@@ -93,8 +93,116 @@ matrix(vector_6,2,2,byrow = T)
 # 给矩阵的行或列命名
 dimnames(matrix_3) <- list(c('a','b'),c('d','e','f'))
 
+# 矩阵元素的选取
+matrix_3[2,3]
+matrix_3[2,]
+matrix_3[,1]
+
+# 矩阵的运算
+3 * matrix_3  # 数乘
+matrix_1 + matrix_3  # 相加
+matrix_1 %*% t(matrix_3)  # 矩阵相乘，保证同型，注意符号
+t(matrix_3)  # 求转置
+matrix_2 <- matrix(c(1,2,3,4),2,2,byrow = T)
+matrix_4 <- matrix(c(4,5,6,7),2,2)
+solve(matrix_2)  # 求逆矩阵
+solve(matrix_2,matrix_4)  # 矩阵求解matrix_2 %*% X = matrix_4
+det(matrix_2)  # 求行列式
+matrix_5 <- diag(c(1,1,1,1))  # 单位阵
+eigen(matrix_2)  # 求特征值
+# combin（）：https://blog.csdn.net/xiaoxiao_ziteng/article/details/114944003
+combn(a,2)  # a中任意两个数的组合
+combn(a,3)
+
+# apply()函数的应用
+apply(matrix_2,1,sum)  # 第二个参数，1代表行，2代表列
+
+# 列合并
+cbind(matrix_1,matrix_2)  # 矩阵合并
+cbind(matrix_1,vector_1)  # 矩阵向量合并
+
+# 行合并
+rbind(matrix_1,matrix_3)
+rbind(matrix_1,vector_1)
+
+# 数组（多维矩阵）
 
 
+# 数据框
+df_1 <- data.frame(matrix_1)
+df_2 <- data.frame(matrix_1,matrix_2)
+
+name <- c('Jack','Mike','Jane','Alen')
+age <- c('20','21','19','18')
+id <- c('001','002','003','004')
+df_student <- data.frame(id,name,age)
+attach(df_student)  # 能直接使用df_student中的特征，例如age
+
+# 列表（将不同类型的数据进行打包）
+list(matrix_1,df_student)
+
+
+######## 1.4 数据处理
+# 保存数据
+write.table(df_student,'data/df_student.txt',row.names = T,col.names = T)
+
+# 读入数据
+df_student2 <- read.table('data/df_student.txt')
+
+# 数据转换
+as.array()  # 转为数组
+as.factor() # 转为数据框
+as.character()  # 转为字符
+as.numeric()  # 转为数值
+as.data.frame()  # 转为数据框
+
+######## 1.5 R编程
+# 判断语句
+i <- 0
+if (a > i) {i + 1} else {print(i)}
+
+# 循环语句
+for (i in 1:4){
+  print(i**2)
+}
+
+while (i < 4){
+  print(i**2)
+  i = i + 1
+}
+
+# 函数的编写和调用
+f1 = function(x){
+  x ** 2
+}
+f1(3)
+
+f2 = function(x,y){
+  return(x ** 2 + y);x + y
+}
+f2(2,9)
+
+f3 <- function(x){
+  x ** 2 + sin(x)
+}
+f3(1)
+
+######### 1.6 统计计算
+# 抽样
+s1 <- sample(100,10)
+s2 <- sample(100,10,replace = T)
+unique(s2)  # x中重复的数据只显示一次
+
+# 统计分布
+# 正态分布:norm(x,mean,sd)
+dnorm(c(0,1,2,3),0,1)  # 密度函数求值
+pnorm(c(0.1,0.2,0.5,0.9),0,1)  # 分布函数求值
+qnorm(c(0.1,0.2,0.5,0.9),0,1)  # 分位数求值
+rnorm(10,0,1)  # 生成正态随机数
+
+# 二项分布
+
+# 超几何分布
 
 
 
